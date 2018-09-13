30124b3494bc581860b731876434be605e319957
M. Imam Pratama <imambungo@gmail.com>
2018-09-13 14:25:31 +0700

    commit

---
+++ tugasfor.php
@@ -0,0 +1,17 @@
+<!-- M. Imam Pratama -->
+<?php
+$angka = 10;
+$kali = 8;
+$bagi = 4;
+$kurang = 6;
+$tambah = 2;
+echo "Aku adalah angka $angka.<br>";
+$angka *= $kali;
+echo "jika aku dikali $kali, jumlahku sekarang $angka.<br>";
+$angka /= $bagi;
+echo "jika aku dibagi $bagi, jumlahku sekarang $angka.<br>";
+$angka -= $kurang;
+echo "jika aku dikurang $kurang, jumlahku sekarang $angka.<br>";
+$angka += $tambah;
+echo "jika aku ditambah $tambah, jumlahku sekarang $angka.<br>";
+?>
