e9e55fdf659d9f8c4930eaf96297109d2a46559c
M. Imam Pratama <imambungo@gmail.com>
2018-09-13 16:20:03 +0700

    oke

--- tugasfor.php
+++ tugasfor.php
@@ -14,4 +14,35 @@ $angka -= $kurang;
 echo "jika aku dikurang $kurang, jumlahku sekarang $angka.<br>";
 $angka += $tambah;
 echo "jika aku ditambah $tambah, jumlahku sekarang $angka.<br>";
+
+function isPrima($a) {
+    for ($i = 2; $i < $a/2; $i++)
+        if ($a%$i == 0)
+            return false;
+    return true;
+}
+
+for ($i=1; $i < 16; $i++) { 
+    echo "angka $i adalah bilangan ";
+    if ($i % 2 == 0)
+        echo "ganjil";
+    else
+        echo "genap";
+    if (isPrima($i) && $i > 1)
+        echo " dan bil prima";
+    echo "<br>";
+}
+
+$n = 8;
+
+for ($i=1; $i <= $n; $i++) { 
+    $k = $i;
+    for ($j=1; $j <= $i; $j++) { 
+        echo "$k\t";
+        $k += $n-$j;
+    }
+    echo "<br>";
+}
+
+
 ?>
